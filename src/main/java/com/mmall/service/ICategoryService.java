94133781bdd139ad2df48319a8e41eeb7780a120
geekerstar <247507792@qq.com>
2018-06-23 13:45:35 +0800

    v1.0.2.2 查询节点和递归查找功能开发 m6.3

--- src/main/java/com/mmall/service/ICategoryService.java
+++ src/main/java/com/mmall/service/ICategoryService.java
@@ -1,6 +1,9 @@
 package com.mmall.service;
 
 import com.mmall.common.ServerResponse;
+import com.mmall.pojo.Category;
+
+import java.util.List;
 
 /**
  * @Author: Geekerstar(jikewenku.com)
@@ -11,4 +14,9 @@ public interface ICategoryService {
     ServerResponse addCategory(String categoryName, Integer parentId);
 
     ServerResponse updateCategoryName(Integer categoryId,String categoryName);
+
+    ServerResponse<List<Category>> getChildrenParallelCategory(Integer categoryId);
+
+    ServerResponse selectCategoryAndChildrenById(Integer categoryId);
+
 }
