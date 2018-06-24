d23105fad57def0f612ee1ca35fe32d7a5e66e05
geekerstar <247507792@qq.com>
2018-06-24 11:10:16 +0800

    v1.0.5.1 收货地址增，删，改，查，分页列表，地址详情功能开发 m9.2

---
+++ src/main/java/com/mmall/service/IShippingService.java
@@ -0,0 +1,23 @@
+package com.mmall.service;
+
+import com.github.pagehelper.PageInfo;
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.Shipping;
+
+/**
+ * @Author: Geekerstar(jikewenku.com)
+ * @Date: 2018/6/24 10:47
+ * @Description:
+ */
+public interface IShippingService {
+    ServerResponse add(Integer userId, Shipping shipping);
+
+    ServerResponse<String> del(Integer userId,Integer shippingId);
+
+    ServerResponse update(Integer userId, Shipping shipping);
+
+    ServerResponse<Shipping> select(Integer userId, Integer shippingId);
+
+    ServerResponse<PageInfo> list(Integer userId, int pageNum, int pageSize);
+
+}
