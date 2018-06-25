c7363319e78799f8715e3952364957259a8057f2
geekerstar <247507792@qq.com>
2018-06-25 11:09:05 +0800

    v1.0.6.2 支付宝支付回调函数实现 m10.8

--- src/main/java/com/mmall/controller/portal/OrderController.java
+++ src/main/java/com/mmall/controller/portal/OrderController.java
@@ -1,10 +1,16 @@
 package com.mmall.controller.portal;
 
+import com.alipay.api.AlipayApiException;
+import com.alipay.api.internal.util.AlipaySignature;
+import com.alipay.demo.trade.config.Configs;
+import com.google.common.collect.Maps;
 import com.mmall.common.Const;
 import com.mmall.common.ResponseCode;
 import com.mmall.common.ServerResponse;
 import com.mmall.pojo.User;
 import com.mmall.service.IOrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,7 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-
+import java.util.Iterator;
+import java.util.Map;
 /**
  * @Author: Geekerstar(jikewenku.com)
  * @Date: 2018/6/25 9:49
@@ -21,6 +28,7 @@ import javax.servlet.http.HttpSession;
 @RestController
 @RequestMapping("/order/")
 public class OrderController {
+    private static final Logger logger = LoggerFactory.getLogger(OrderController.class);
 
     @Autowired
     private IOrderService iOrderService;
@@ -36,4 +44,37 @@ public class OrderController {
         return iOrderService.pay(orderNo, user.getId(), path);
     }
 
+    @RequestMapping("alipay_callback.do")
+    @ResponseBody
+    public Object alipayCallback(HttpServletRequest request) {
+        Map<String, String> params = Maps.newHashMap();
+
+        Map requestParams = request.getParameterMap();
+        for (Iterator iter = requestParams.keySet().iterator(); iter.hasNext(); ) {
+            String name = (String) iter.next();
+            String[] values = (String[]) requestParams.get(name);
+            String valueStr = "";
+            for (int i = 0; i < values.length; i++) {
+                valueStr = (i == values.length - 1) ? valueStr + values[i] : valueStr + values[i] + ",";
+            }
+            params.put(name, valueStr);
+        }
+        logger.info("支付宝回调,sign:{},trade_status:{},参数:{}", params.get("sign"), params.get("trade_status"), params.toString());
+
+        //非常重要的一点，验证回调的正确性（确认是支付宝发送的），并且避免重复通知
+        params.remove("sign_type");
+        try {
+            boolean alipayRSACheckedV2 = AlipaySignature.rsaCheckV2(params, Configs.getAlipayPublicKey(), "utf-8", Configs.getSignType());
+
+            if (!alipayRSACheckedV2) {
+                return ServerResponse.createByErrorMessage("非法结束，验证不通过，再恶意请求将报警。");
+            }
+
+        } catch (AlipayApiException e) {
+            logger.error("支付宝验证回调异常", e);
+        }
+
+        //todo 验证各种数据，下一节再做
+        return null;
+    }
 }
