8a9dbec39b4775a274ebf5c1e54ea648a7706bb4
geekerstar <247507792@qq.com>
2018-07-23 17:03:43 +0800

    Spring Schedule实现定时关单

---
+++ src/main/java/com/mmall/task/CloseOrderTask.java
@@ -0,0 +1,29 @@
+package com.mmall.task;
+
+import com.mmall.service.IOrderService;
+import com.mmall.util.PropertiesUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+/**
+ * @Author: Geekerstar(jikewenku.com)
+ * @Date: 2018/7/23 16:42
+ * @Description:定时关单
+ */
+@Component
+@Slf4j
+public class CloseOrderTask {
+    @Autowired
+    private IOrderService iOrderService;
+
+    //    @Scheduled(cron="0 */1 * * * ?")//每1分钟(每个1分钟的整数倍)
+    public void closeOrderTaskV1(){
+        log.info("关闭订单定时任务启动");
+        int hour = Integer.parseInt(PropertiesUtil.getProperty("close.order.task.time.hour","2"));
+//        iOrderService.closeOrder(hour);
+        log.info("关闭订单定时任务结束");
+    }
+
+}
